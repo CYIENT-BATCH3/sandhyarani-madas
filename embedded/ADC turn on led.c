ac4c1e3357bda0f344510f4c85b8ab35357c2625
sandhya123M <111977186+sandhya123M@users.noreply.github.com>
2022-09-21 22:05:52 +0530

    Add files via upload

---
+++ embedded/ADC turn on led.c
@@ -0,0 +1,98 @@
+//program using ADC and based on the conditions turning on the leds
+
+
+
+*start
+*setting input and output port
+*connecting ADC
+*based on conditions turning on leds
+
+
+// PIC18F458 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1H
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
+#pragma config OSCS = OFF       // Oscillator System Clock Switch Enable bit (Oscillator system clock switch option is disabled (main oscillator is source))
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = OFF        // Brown-out Reset Enable bit (Brown-out Reset disabled)
+#pragma config BORV = 25        // Brown-out Reset Voltage bits (VBOR set to 2.5V)
+
+// CONFIG2H
+#pragma config WDT = ON         // Watchdog Timer Enable bit (WDT enabled)
+#pragma config WDTPS = 128      // Watchdog Timer Postscale Select bits (1:128)
+
+// CONFIG4L
+#pragma config STVR = OFF       // Stack Full/Underflow Reset Enable bit (Stack Full/Underflow will not cause Reset)
+#pragma config LVP = OFF        // Low-Voltage ICSP Enable bit (Low-Voltage ICSP disabled)
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000200-001FFFh) not code protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) not code protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) not code protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) not code protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot Block (000000-0001FFh) not code protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000200-001FFFh) not write protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) not write protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) not write protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) not write protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0001FFh) not write protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000200-001FFFh) not protected from Table Reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from Table Reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from Table Reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from Table Reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0001FFh) not protected from Table Reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+#define __XTALTAL_FREQ 20000000//defining external oscilaltor frequency
+void main()
+{
+   unsigned int a=0;//declaring variable
+    TRISE=0x02;//setting Eport as input
+    TRISC=0x00;//setting PORTC AS output
+    PORTC=0x00;//initialising PORTC as low
+    ADCON0=0x31;//configuring ADCON0 for channe6
+    ADCON1=0x80;//configuring ADCON1
+    while(1)
+    {
+        for(long int i=0;i<30000;i++);//delay
+        GO_nDONE=1;//assigning 1
+        while(GO_nDONE);
+        a=(ADRESH<<8)+ADRESL;//storing data in a
+        if(a>0&&a<200)//check if a between 0 to 200
+        {
+            PORTC=0x01;//LED 1 turns on
+        }   
+        else if(300<a&&a<600)
+//checking a between 300 and 600
+        {
+            PORTC=0x02;//LED 2 turning on
+        }   
+        else if(800<a&&a<1000)//checking a bewteen 800 and 1000
+        {
+            PORTC=0x03;//LED 1&2turning ON
+        }   
+
+
+    }
+}
