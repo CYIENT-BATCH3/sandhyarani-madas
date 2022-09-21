b18eb23413b8679785ac9c648210190a4325cfa6
sandhya123M <111977186+sandhya123M@users.noreply.github.com>
2022-09-21 23:43:09 +0530

    Add files via upload

---
+++ week5/program for reading a num in c++.c
@@ -0,0 +1,14 @@
+//program for reading a number and  print the output 
+
+
+
+#include<iostream>//input output operation
+using namespace std;//every std function  and literals variable are defined in std namespace
+int main()
+{
+	int a;//declaring variable
+	setbuf(stdout,NULL);// printning the output in console(its for compiler purpose)
+	cout<<"enter a number"<<endl;
+        cin>>a;//reading input
+        cout<<"a =="<<a;//printing output
+}
